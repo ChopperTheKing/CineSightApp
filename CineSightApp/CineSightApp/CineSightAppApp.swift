63aa9cc8b392f1b9f0a18bf604a14f7670687ab3
Ronnie Kissos <vvxn8kgmp4@privaterelay.appleid.com>
2023-10-05 10:10:56 -0400

    Initial commit

---
+++ CineSightApp/CineSightApp/CineSightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineSightAppApp.swift
+//  CineSightApp
+//
+//  Created by Ronnie Kissos on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CineSightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
